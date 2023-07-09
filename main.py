094be2ef65017e8bfaa0e45d53a4ba933fb8b9cc
Luis Altuve <luis2ra@gmail.com>
2023-07-09 16:42:02 -0500

    class 08: ciclos de request y response

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     mi_ip = request.remote_addr
-    return f"<p>Hello World from Flask!!!</p> Tu IP es {mi_ip}"
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', mi_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello_world():
+    user_ip = request.cookies.get('user_ip')
+    return f"<p>Hello World from Flask!!!</p> Tu IP es {user_ip}"
