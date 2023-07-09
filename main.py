fb4bb1830e6c01200ae1df44114a2d2361631e77
Luis Altuve <luis2ra@gmail.com>
2023-07-09 17:19:50 -0500

    class 10: estructuras de control

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
 @app.route('/')
 def index():
@@ -17,4 +18,15 @@ def index():
 @app.route('/hello')
 def hello_world():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip=user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.html', **context)  # importante la referencia
+
+'''
+Importante la referencia que hace en el pase de esta variable **context,
+que python utiliza para expandir las variables
+
+Opcionalmente, se puede haber manejado con la sintaxix context=context
+'''
