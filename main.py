edeb3098e3fad81e3128aa2bf7f17488208342c2
Luis Altuve <luis2ra@gmail.com>
2023-07-10 11:27:36 -0500

    class 18: Flask-WTF usando el metodo POST

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +12,12 @@ app.config['SECRET_KEY'] = 'SUPER_SECRETO'
 # print(app.config)  # veo las variables de configuracion de la instancia de app Flask
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -37,15 +47,28 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
     # user_ip = request.cookies.get('user_ip') # lo cambiamos por session
     user_ip = session.get('user_ip')
-    
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)  # importante la referencia
 
 '''
